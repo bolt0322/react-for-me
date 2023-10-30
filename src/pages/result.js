c2a50f65959f021ab747d198e4f748fbcb09edf2
bolt0322 <wls324jh@naver.com>
2023-10-30 09:53:55 +0900

    done

--- src/pages/result.js
+++ src/pages/result.js
@@ -14,7 +14,7 @@ function Result(){
     const nickname = location.state.name;
     return(
         <div className={banana.page}>
-                <div className={banana.testResult}>"{nickname}" 대한 나의 점수는?</div>
+                <div className={banana.testResult}>"{nickname}" 에 대한 나의 점수는?</div>
                 <div className={banana.testScore}>80점!</div>
                 <Btn ><div className={banana.myPgEnd}><Link to={"/"}  style={{ textDecoration: "none",color: "white"}}>메뉴로 돌아가기</Link></div></Btn>
             
